922c345a2b4c2d1106b0280de03355bc720d205a
NII-John Jacob C. Coluna <nii-jjccoluna@headoffice.corp.bpi.com.ph>
2025-12-04 15:14:39 +0800

    M1_Activity5

---
+++ M1_Activity5.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package m1javaactivity;
+
+/**
+ * 
+ */
+public class M1_Activity5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for (int i = 1; i <= 50; i++)
+		{
+			sum +=i;
+		}
+		System.out.print("Sum = "+sum);
+	}
+
+}
