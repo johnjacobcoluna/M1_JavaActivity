1b1a20bbf8fe0577082d33b1c9e90e24aede8b8b
NII-John Jacob C. Coluna <nii-jjccoluna@headoffice.corp.bpi.com.ph>
2025-12-05 17:28:19 +0800

    M2_Activity4

---
+++ Executable.java
@@ -0,0 +1,9 @@
+package M2_OOP_Activity;
+
+
+public interface Executable {
+	
+		void run();
+		void stop();
+
+}
---
+++ MSExcel.java
@@ -0,0 +1,19 @@
+package M2_OOP_Activity;
+
+public class MSExcel implements Executable{
+	
+	@Override
+	public void run()
+	{
+		String message = "Opening MS Excel...";
+		System.out.println(message);
+	}
+	
+	@Override
+	public void stop()
+	{
+		String message = "Stopping MS Excel...";
+		System.out.println(message);
+	}
+
+}
